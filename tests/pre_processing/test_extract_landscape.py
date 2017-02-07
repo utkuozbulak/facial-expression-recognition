111420538a6fe0e8f16fd6bd8fd00674b61f0332
Alex Young <alexdy2007@gmail.com>
2017-02-07 10:55:05 +0000

    got rid of pep8 error in test preprocessing

--- tests/pre_processing/test_extract_landscape.py
+++ tests/pre_processing/test_extract_landscape.py
@@ -1,13 +1,15 @@
-import unittest
-from pre_processing.extract_landscape import get_facial_vectors
 import logging
+import unittest
+
+from src.pre_processing.extract_landscape import get_facial_vectors
+
 
 class TestExtractLandScape(unittest.TestCase):
-
     logging.basicConfig(level=logging.INFO)
+
     def test_get_all_vectors(self):
         all_vectors = get_facial_vectors()
-        self.assertEqual(all_vectors.shape, (35886,68,2))
+        self.assertEqual(all_vectors.shape, (35886, 68, 2))
 
 
 if __name__ == '__main__':
